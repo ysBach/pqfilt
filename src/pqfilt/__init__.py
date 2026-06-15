c761f137bf9309f70d267a1320c28f0ce76aad5f
ysBach <ysbach93@gmail.com>
2026-06-15 16:44:37 +0900

    expose to_pyarrow_expr

--- src/pqfilt/__init__.py
+++ src/pqfilt/__init__.py
@@ -11,6 +11,15 @@ Usage::
 from __future__ import annotations
 
 from .core import read, filter_df
-from ._parser import parse_expression, FilterExpr, AndExpr, OrExpr, NotExpr
+from ._parser import parse_expression, to_pyarrow_expr, FilterExpr, AndExpr, OrExpr, NotExpr
 
-__all__ = ["read", "filter_df", "parse_expression", "FilterExpr", "AndExpr", "OrExpr", "NotExpr"]
+__all__ = [
+    "read",
+    "filter_df",
+    "parse_expression",
+    "to_pyarrow_expr",
+    "FilterExpr",
+    "AndExpr",
+    "OrExpr",
+    "NotExpr",
+]
